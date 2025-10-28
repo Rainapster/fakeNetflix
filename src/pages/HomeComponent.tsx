32ac4ce25f6afca203912c954d0ff1e464c61dd8
Rainapster <raitre.3@gmail.com>
2025-10-28 12:40:29 +0100

    remove button back from favorite page, add film and series to homepage

--- src/pages/HomeComponent.tsx
+++ src/pages/HomeComponent.tsx
@@ -1,6 +1,11 @@
+import { Movies } from "./Movies"
+import { Series } from "./Series"
+
 export const HomeComponent = () =>{
     return(
         <>
+            <Movies/>
+            <Series/>
         </>
     )
 }
